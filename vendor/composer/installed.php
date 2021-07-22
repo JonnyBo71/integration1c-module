731495843f1bb390d11417dd6d6c4952dadde557
JonnyBo <526@talp.ru>
2021-07-22 16:41:27 +0300

    последние исправления

---
+++ vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'laravel-module',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '89e9a024f904d9e9009111f32bbfecf679ef84b3',
+        'name' => 'altrpjb/integration1c-module',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'altrpjb/integration1c-module' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'laravel-module',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '89e9a024f904d9e9009111f32bbfecf679ef84b3',
+            'dev_requirement' => false,
+        ),
+        'arsengoian/commerce-ml' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../arsengoian/commerce-ml',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'reference' => 'a07dc7204801bc5ebdc2666030a8e8d0fafa1f8d',
+            'dev_requirement' => false,
+        ),
+        'carono/commerceml' => array(
+            'pretty_version' => '0.2.5',
+            'version' => '0.2.5.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../carono/commerceml',
+            'aliases' => array(),
+            'reference' => '9d357db9912ab680bc3932ba7c77a6dbb53c8e42',
+            'dev_requirement' => false,
+        ),
+    ),
+);
